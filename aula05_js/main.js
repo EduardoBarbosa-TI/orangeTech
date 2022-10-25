9d428b34a7acc45e0c027dd68b95a418f3a7e84f
eduardobarbosa-ti <eduardobb@rede.ulbra.br>
2022-10-25 00:32:05 -0300

    Desafio lógico realizado

---
+++ aula05_js/main.js
@@ -0,0 +1,20 @@
+const {gets, print} = require("./script.js");
+
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+
+
+
+
+const eduardo = {
+    nome:'eduardo'
+};
+
+// const { nome } = pessoa;
+const nome = pessoa.nome;
+
+print(gets());
