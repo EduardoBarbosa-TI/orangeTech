1f4687d1efbaa4e2adb9e3c17ec2b31e81e474cc
eduardobarbosa-ti <eduardobb@rede.ulbra.br>
2022-11-09 00:02:14 -0300

    Criando projeto GitHub Wiki com React + Axios + styled-components

---
+++ desafios_react/github-wiki/src/components/ItemRepo/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ItemContainer } from './style'
+
+function ItemRepo({repo, handleRemoveRepo}) {
+  const handleRemove = () => {
+    handleRemoveRepo(repo.id)
+  }
+  return (
+    <ItemContainer onClick={handleRemove}>
+        <h3>{repo.name}</h3>
+        <p>{repo.full_name} </p>
+        <a href={repo.html_url} rel="noreferrer" target="_blank" >Visitar</a><br/>
+      
+        <a href='#' rel="noreferrer">Remover</a>
+        <hr/>
+    </ItemContainer>
+  )
+}
+
+export default ItemRepo
